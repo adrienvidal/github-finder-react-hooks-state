919885a05c39d12dc9f1e41dfd999635a3e79254
Adrien Vidal <adriensg.vidal@gmail.com>
2020-10-26 19:51:39 +0100

    update search with api

--- src/App.js
+++ src/App.js
@@ -8,16 +8,28 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
-    loading: true,
+    loading: false,
   };
 
   async componentDidMount() {
+    this.setState({ loading: true });
     const res = await axios.get(
       `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
     this.setState({ users: res.data, loading: false });
   }
+
+  // Search github users
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+    
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: res.data.items, loading: false });
+  };
 
   render() {
     const { users, loading } = this.state;
@@ -26,7 +38,7 @@ class App extends Component {
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Search />
+          <Search searchUsers={this.searchUsers} />
           <Users loading={loading} users={users} />
         </div>
       </div>
