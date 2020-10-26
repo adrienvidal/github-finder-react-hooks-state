0b081a3ef2989dd027b276bd6bbce10c73d4afd2
Adrien Vidal <adriensg.vidal@gmail.com>
2020-10-26 20:10:55 +0100

    update search with clear button

--- src/App.js
+++ src/App.js
@@ -23,13 +23,15 @@ class App extends Component {
   // Search github users
   searchUsers = async (text) => {
     this.setState({ loading: true });
-    
+
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
     this.setState({ users: res.data.items, loading: false });
   };
+
+  clearUsers = () => this.setState({ users: [], loading: false });
 
   render() {
     const { users, loading } = this.state;
@@ -38,7 +40,11 @@ class App extends Component {
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Search searchUsers={this.searchUsers} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+          />
           <Users loading={loading} users={users} />
         </div>
       </div>
