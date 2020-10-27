2d00793dc1b97f9028b82be54a558176eff77c4e
Adrien Vidal <adriensg.vidal@gmail.com>
2020-10-27 17:52:09 +0100

    refacto app with hooks

--- src/components/users/Search.jsx
+++ src/components/users/Search.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const Search = ({ searchUsers, setAlert, showClear, clearUsers }) => {
+const Search = ({ searchUsers, showAlert, showClear, clearUsers }) => {
   const [text, setText] = useState('');
 
   const onChange = (e) => setText(e.target.value);
@@ -10,7 +10,7 @@ const Search = ({ searchUsers, setAlert, showClear, clearUsers }) => {
     e.preventDefault();
 
     if (text === '') {
-      setAlert('Please enter something', 'light');
+      showAlert('Please enter something', 'light');
     } else {
       searchUsers(text);
       setText('');
@@ -47,7 +47,7 @@ Search.propTypes = {
   searchUsers: PropTypes.func.isRequired,
   clearUsers: PropTypes.func.isRequired,
   showClear: PropTypes.bool.isRequired,
-  setAlert: PropTypes.func.isRequired,
+  showAlert: PropTypes.func.isRequired,
 };
 
 export default Search;
