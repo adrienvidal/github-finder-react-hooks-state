bb33a0bf74e1f59f0f6a4ba78cfad145a6c390d2
Adrien Vidal <adriensg.vidal@gmail.com>
2020-10-27 12:15:25 +0100

    add alert

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Search from './components/users/Search';
 import Users from './components/users/Users';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
 
@@ -9,6 +10,7 @@ class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
   async componentDidMount() {
@@ -31,19 +33,32 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  // clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
 
+  // set alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg: msg, type: type }, loading: false });
+
+    setTimeout(() => {
+      this.setState({ alert: null });
+    }, 5000);
+  };
+
   render() {
-    const { users, loading } = this.state;
+    const { users, loading, alert } = this.state;
 
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
+          <Alert alert={alert} />
+
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>
